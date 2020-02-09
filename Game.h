808f7849218fdf1a177d295122276508e597df28
Klyta <55992278+Klyta@users.noreply.github.com>
2020-02-09 22:25:51 +0200

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include "vector"
+#include "Player.h"
+class Game
+{
+	std::vector<Player*> players;
+public:
+
+	Player* GetPlayer(int);
+	void SavePlayer(Player*);
+};
+
